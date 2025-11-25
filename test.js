eec5d81d6497b1829a652b164911aecb92c596b0
Yajing <2946547864@qq.com>
2025-11-25 17:23:12 -0600

    Day1-16

---
+++ test.js
@@ -0,0 +1,41 @@
+/*
+//Input & Output (alert, prompt have highest excute order)
+
+alert('hello js')
+document.write("I'm here")
+
+console.log("magic")
+
+prompt('enter your age')
+
+//; can be moved
+
+
+// variable & constant
+let variable1 = 10, variable2 = prompt('enter your age')
+console.log(variable1)
+document.write(variable2)
+
+const PI = 3.14
+console.log(PI)
+
+
+let num1 = 10, num2 = 20
+[num1, num2] = [num2, num1]
+
+
+
+//Array + Template Literal: ` ${} ` like <f string   in python>
+let numbers = [10,1,23,55,50]
+console.log(`The number array is ${numbers} and the array length is ${numbers.length}`)
+
+
+// data type: undefined have't value; null have value but programer set an empty value, like<None in python>.
+console.log(undefined + 1)      // NaN
+console.log(null + 1)       // 1
+
+// typeof null === "object"   // 历史遗留 bug
+let name = 'pink'
+console.log(typeof name)        /string
+*/
+
